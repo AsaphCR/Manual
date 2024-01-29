39c551e1c3fbba3a916f52c8ea1af1c2fab46c08
Asaph Gil <asaph.gil@carolrossato.com.br>
2024-01-29 09:09:10 -0300

    Correção de erro de chamada em Loop por endereçamento errado na função loadPage e atualizações de paginas

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,13 +9,13 @@ function loadPage(path) {
             if (!response.ok) {
                 throw new Error('Page not found');
             }
-            return response.text(); // Extract the text from the response
-        })
+            return response.text();
+        }) // Extract the text from the response
         .then(text => {
             contentSection.innerHTML = text;
         }) // Dump the text from the response in the contentSection
         .catch(error => {
             console.error('Error:', error);
-            loadPage('404');
+            loadPage('pages/404');
         }); // Handle any errors
 }
--- assets/scripts/navigationMenu.js
+++ assets/scripts/navigationMenu.js
@@ -1,10 +1,14 @@
+// let menuErp, menuPdv;
+
 function navIndex(element, path) {
     fetch(`${path}.html`)
         .then(response => response.text()) // Extract the text from the response
         .then(text => {
             element.innerHTML = text;
         }) // Call the click function with the text
-        .catch(error => console.error('Error fetching data:', error)); // Handle any errors
+        .catch(error => {console.error('Error fetching data:', error)}); // Handle any errors
 }
 
+// menuErp = fetch('erpIndex.html').then(response => {if (!response.ok) {throw new Error('Network response was not ok');} return response.text();}).then(text => {console.log('Fetched ERP content:', text); menuErp = text; return text;}).catch(error => {console.error('Error fetching ERP data:', error);});
 
+// menuPdv = fetch(`${path}.html`).then(response => response.text()).then(text => {return text;}).catch(error => {console.error('Error fetching data:', error)});
