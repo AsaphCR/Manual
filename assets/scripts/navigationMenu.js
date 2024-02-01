6ab11366e99f4bd29be840e10503562eb579764c
Asaph Gil <asaph.gil@carolrossato.com.br>
2024-02-01 17:00:04 -0300

    1.0.0 - primeiro controle e lançamento inicial. Página de entrada de nota revisada.

--- assets/scripts/navigationMenu.js
+++ assets/scripts/navigationMenu.js
@@ -2,8 +2,10 @@
 let erp = 'pages/erp', entradaDeNotas = 'pages/entradaDeNotas', emissaoDeNotas = 'pages/emissaoDeNotas', transferencia = 'pages/transferencia', transferenciaEntreLojas = 'pages/transferenciaEntreLojas', transferenciaParaMatriz = 'pages/transferenciaParaMatriz', devolucao = 'pages/devolucao', malinha = 'pages/malinha', demonstracao = 'pages/demonstracao';
 
 // PDV paths
+let pdv = 'pages/pdv', venda = 'pages/venda', cancelamentoCupom = 'pages/cancelamentoCupom', reimpressaoCupom = 'pages/reimpressaoCupom';
 
 // Troca paths
+let troca = 'pages/troca', trocaNaLoja = 'pages/trocaNaLoja', trocaEntreLojas = 'pages/trocaEntreLojas';
 
 // Pré-venda paths
 
