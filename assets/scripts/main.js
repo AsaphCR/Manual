cc565005ce684e0d9307c00d12f7c8918c1d37d9
Asaph Gil <asaph.gil@carolrossato.com.br>
2024-02-19 11:09:31 -0300

    1.3.1 - Scroll features adicionadas.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,6 @@
 // Elementos
 let contentSection = document.querySelector('section');
+let scrollToTop = document.querySelector('.scrollToTop');
 
 
 // Funções
@@ -18,4 +19,17 @@ function loadPage(path) {
             console.error('Error:', error);
             loadPage('pages/404');
         }); // Handle any errors
+    window.scrollTo(0, 0);
 }
+
+function scrollToTopButton() {
+    if (window.scrollY == 0) {
+        scrollToTop.style.display = 'none';
+    } else {
+        scrollToTop.style.display = 'flex';
+    }
+}
+
+
+// Event Listeners
+window.addEventListener('scroll', scrollToTopButton)
